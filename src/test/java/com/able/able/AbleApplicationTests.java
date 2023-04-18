2661b0a1a0a8ceeb5f7b36d5a233414407f00d8c
Amanda Ribeiro <aamandaribeirocosta@gmail.com>
2023-04-18 09:47:46 -0300

    Projeto Spring criado

---
+++ src/test/java/com/able/able/AbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.able.able;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
