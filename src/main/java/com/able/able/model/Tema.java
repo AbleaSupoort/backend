217dd3905472ab0744914881ceb9cc59fad0cd25
Thiago Trevisani <Thiagoatkl@gmail.com>
2023-04-18 10:43:48 -0300

    Task 3

---
+++ src/main/java/com/able/able/model/Tema.java
@@ -0,0 +1,50 @@
+package com.able.able.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "O Atributo título é Obrigatório!")
+	@Size(min = 5, max = 500, message = "O atributo título deve ter no minimo 05 e no máximo 500 caracteres")
+	private String titulo;
+
+	@NotNull(message = "O Atributo Descrição é obrigatório")
+	@Size(min = 5, max = 1000, message = "O atributo descrição deve ter no minimo 05 e no máximo 1000 caracteres")
+	private String descricao;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return this.titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
