77c23fa2a305473b10429d3f2c56826a42d858fc
CarolFAraujo <carolina.fac@gmail.com>
2023-04-24 11:14:36 -0300

    CRUD Tema 02

--- src/main/java/com/able/able/controller/TemaController.java
+++ src/main/java/com/able/able/controller/TemaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,23 +23,34 @@ import jakarta.validation.Valid;
 @RequestMapping("/temas")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class TemaController {
-	
+
 	@Autowired
 	private TemaRepository temaRepository;
 
-	
+	@GetMapping("/all")
+	public ResponseEntity<List<Tema>> getAll() {
+		return ResponseEntity.ok(temaRepository.findAll());
+	}
+
 	// Procurar tema por titulo
 	@GetMapping("/titulo/{titulo}")
-	public ResponseEntity <List<Tema>> getByTitulo (@PathVariable String titulo){
+	public ResponseEntity<List<Tema>> getByTitulo(@PathVariable String titulo) {
 		return ResponseEntity.ok(temaRepository.findAllByTituloContainingIgnoreCase(titulo));
 	}
-	
-	
+
 	// Cadastrar tema
 	@PostMapping
-	public ResponseEntity <Tema> post (@Valid @RequestBody Tema tema){
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(temaRepository.save(tema));
+	public ResponseEntity<Tema> post(@Valid @RequestBody Tema tema) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(temaRepository.save(tema));
+
 	}
-	
+
+	@PutMapping
+	public ResponseEntity<Tema> put(@Valid @RequestBody Tema tema) {
+
+		return temaRepository.findById(tema.getId())
+				.map(resposta -> ResponseEntity.ok().body(temaRepository.save(tema)))
+				.orElse(ResponseEntity.notFound().build());
+
+	}
 }
