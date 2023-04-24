20328eb6054b2b479b6a9d0c8fd7350daa9b3fe9
Amanda Ribeiro <aamandaribeirocosta@gmail.com>
2023-04-24 11:48:29 -0300

    CRUD Tema V3

--- src/main/java/com/able/able/controller/TemaController.java
+++ src/main/java/com/able/able/controller/TemaController.java
@@ -1,18 +1,22 @@
 package com.able.able.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.able.able.model.Tema;
 import com.able.able.repository.TemaRepository;
@@ -47,10 +51,20 @@ public class TemaController {
 
 	@PutMapping
 	public ResponseEntity<Tema> put(@Valid @RequestBody Tema tema) {
-
 		return temaRepository.findById(tema.getId())
 				.map(resposta -> ResponseEntity.ok().body(temaRepository.save(tema)))
 				.orElse(ResponseEntity.notFound().build());
 
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional <Tema> tema = temaRepository.findById(id);
+		
+		if (tema.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		temaRepository.deleteById(id);
 	}
 }
