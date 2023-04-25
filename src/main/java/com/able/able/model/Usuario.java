2ce22232719e7ca96bce3b052143fd4deadea500
Amanda Ribeiro <aamandaribeirocosta@gmail.com>
2023-04-25 11:05:23 -0300

    Task5 VFinal

--- src/main/java/com/able/able/model/Usuario.java
+++ src/main/java/com/able/able/model/Usuario.java
@@ -1,9 +1,14 @@
 package com.able.able.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -29,6 +34,9 @@ public class Usuario {
 	private String foto;
 
 	
+	@OneToMany
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
 	
 	public Long getId() {
 		return id;
@@ -68,6 +76,14 @@ public class Usuario {
 
 	public void setFoto(String foto) {
 		this.foto = foto;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
 	}
 	
 	
