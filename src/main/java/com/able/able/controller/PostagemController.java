9df8b91427ba8e1b14402bdae2253d91f027188b
Amanda Ribeiro <aamandaribeirocosta@gmail.com>
2023-04-25 10:14:22 -0300

    crud postagens

--- src/main/java/com/able/able/controller/PostagemController.java
+++ src/main/java/com/able/able/controller/PostagemController.java
@@ -3,9 +3,11 @@ package com.able.able.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,15 @@ public class PostagemController {
 		return ResponseEntity.ok(postagemRepository.findAll());
 		// SELECT * FROM tb_postagens
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		// SELECT * FROM tb_postagens WHERE id = ?
+	}
+	
+	
 
 }
