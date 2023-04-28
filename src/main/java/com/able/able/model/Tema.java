a314fdacec43f0d1b86c2c1ca034374c6c4fc245
Jamile Farias <fariasjamile1234@gmail.com>
2023-04-28 09:51:12 -0300

    Implatação da Spring Security v1

--- src/main/java/com/able/able/model/Tema.java
+++ src/main/java/com/able/able/model/Tema.java
@@ -1,16 +1,9 @@
 package com.able.able.model;
 
-import java.util.List;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -29,10 +22,6 @@ public class Tema {
 	@NotNull(message = "O Atributo Descrição é obrigatório")
 	@Size(min = 5, max = 1000, message = "O atributo descrição deve ter no minimo 05 e no máximo 1000 caracteres")
 	private String descricao;
-
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "tema", cascade = CascadeType.REMOVE)
-	@JsonIgnoreProperties("tema")
-	private List<Postagem> postagem;
 
 	public Long getId() {
 		return this.id;
@@ -57,15 +46,5 @@ public class Tema {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-
-	public List<Postagem> getPostagem() {
-		return postagem;
-	}
-
-	public void setPostagem(List<Postagem> postagem) {
-		this.postagem = postagem;
-	}
-	
-	
 
 }
